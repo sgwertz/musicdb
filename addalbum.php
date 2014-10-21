5a2abeb91d7fe7644246b99973ea181b2a1c3d05
sgwertz <sgw4@zips.uakron.edu>
2014-10-20 21:11:02 -0400

    addAlbum

---
+++ addalbum.php
@@ -0,0 +1,18 @@
+<?php
+require 'sql/config.php';
+$con=mysqli_connect($dbserv,$dbuser,$dbpass,$dbbase);
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$sql="INSERT INTO albums (artist_id, title, year) VALUES ((SELECT id FROM artists
+WHERE id = {$_GET['artistname']}), {$_GET['albumname']}, {$_GET['year']})";
+
+if (!mysqli_query($con,$sql)) {
+  die('Error: ' . mysqli_error($con));
+}
+echo "Album added";
+
+mysqli_close($con);
+?>
