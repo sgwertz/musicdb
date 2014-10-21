64cb2aa0273cb0c4319ca4b91b9813cb3db52120
sgwertz <sgw4@zips.uakron.edu>
2014-10-20 21:11:15 -0400

    modArtist and modAlbum

---
+++ php/modalbum.php
@@ -0,0 +1,15 @@
+<?php
+require '../sql/config.php';
+$con=mysqli_connect($dbserv,$dbuser,$dbpass,$dbbase);
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$sql=mysqli_query($con, "UPDATE album SET name = {$_GET['correctName']} WHERE name = {$_GET['originalName']}");
+
+echo "Album modified";
+
+mysqli_close($con);
+
+?>
