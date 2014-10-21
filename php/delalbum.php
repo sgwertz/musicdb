c76f329b4ae191d9ab0892e0b9bc444923e12ef8
sgwertz <sgw4@zips.uakron.edu>
2014-10-20 21:10:26 -0400

    Fixed delAlbum sql connection

--- php/delalbum.php
+++ php/delalbum.php
@@ -1,6 +1,7 @@
 <?php
-$con=mysqli_connect("localhost","root","","musicDB");
-// Open connection
+require '../sql/config.php';
+$con=mysqli_connect($dbserv,$dbuser,$dbpass,$dbbase);
+// Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
@@ -9,9 +10,6 @@ mysqli_query($con,"DELETE FROM tracks WHERE album_id = (SELECT id FROM albums
 WHERE title = 'albumname')");
 mysqli_query($con,"DELETE FROM albums WHERE title = 'albumname'");
 
-if (!mysqli_query($con,$sql)) {
-  die('Error: ' . mysqli_error($con));
-}
 echo "Album deleted";
 
 mysqli_close($con);
