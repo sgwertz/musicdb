1e79d3bd68d644aca1aa9a66061b4dbd96f075ad
Braden Obrzut <admin@maniacsvault.net>
2014-11-20 13:10:17 -0500

    Provide a list of rarities with tracking.

--- php/viewcollection.php
+++ php/viewcollection.php
@@ -13,8 +13,10 @@ while(($row = mysqli_fetch_assoc($result)))
 {
 	if($last_artistid != $row['artist_id'])
 	{
+		if($last_artistid != -1)
+			echo "</fieldset>";
 		// Print the name of the artist when it changes over (artists are sorted)
-		echo "<h1><a href=\"viewalbumsbyArtist.php?artistid={$row['artist_id']}\">{$row['name']}</a></h1>";
+		echo "<fieldset><legend><h1><a href=\"viewalbumsbyArtist.php?artistid={$row['artist_id']}\">{$row['name']}</a></h1></legend>";
 		$last_artistid = $row['artist_id'];
 
 		// Grab the completion percentage.
@@ -28,6 +30,25 @@ while(($row = mysqli_fetch_assoc($result)))
 		mysqli_free_result($result2);
 
 		echo "<p>Completion: {$stats->have}/{$stats->total} (".floor($stats->have*100/$stats->total).'%)</p>';
+
+		// Rarities (various artists tracks)
+		$sql = "SELECT album_id, release_id, number, tracks.title AS title, length, albums.title AS album_title, release_id IN (SELECT release_id FROM collections WHERE owner_id = $uid) AS have FROM tracks INNER JOIN albums ON album_id = albums.id WHERE tracks.artist_id = {$row['artist_id']} AND album_id IN (SELECT id FROM albums WHERE artist_id = 1)";
+		if(!($result2 = mysqli_query($con, $sql)))
+			die('Query error: '.mysqli_error($con));
+		if(mysqli_num_rows($result2))
+		{
+			echo "<p>Rarities:</p><table><tr><th>Album</th><th>Title</th><th>Length</th></tr>";
+			while(($r = mysqli_fetch_assoc($result2)))
+			{
+				if($r['have'])
+					echo "<tr style=\"background-color: #00DD00\">";
+				else
+					echo "<tr>";
+				echo "<td><a href=\"viewalbum.php?albumid={$r['album_id']}\">{$r['album_title']}</a></td><td>{$r['title']}</td><td>{$r['length']}</tr>";
+			}
+			echo "</table>";
+		}
+		mysqli_free_result($result2);
 	}
 
 	echo "<h2>{$row['year']} - <a href=\"viewalbum.php?albumid={$row['album_id']}\">{$row['title']}</a></h2>";
@@ -39,9 +60,9 @@ while(($row = mysqli_fetch_assoc($result)))
 	if(mysqli_num_rows($result2))
 	{
 		echo "<p>You need:</p><ul>";
-		while(($row = mysqli_fetch_assoc($result2)))
+		while(($r = mysqli_fetch_assoc($result2)))
 		{
-			echo "<li>{$row['description']}</li>";
+			echo "<li>{$r['description']}</li>";
 		}
 		echo "</ul>";
 	}
@@ -51,6 +72,8 @@ while(($row = mysqli_fetch_assoc($result)))
 	}
 	mysqli_free_result($result2);
 }
+if($last_artistid != -1)
+	echo "</fieldset>";
 
 mysqli_free_result($result);
 
