a25ac34156b79a537c3292287b6904df7c6b1748
Braden Obrzut <admin@maniacsvault.net>
2014-12-02 12:46:46 -0500

    Small fixes for addrelease.

--- addrelease.php
+++ addrelease.php
@@ -6,7 +6,7 @@ require 'php/header.php';
 <h1>Add A Release</h1>
 
 <form id="addRelease" method="get" action="/php/addrelease.php">
-  Description: <textarea name="description" id="description"></textarea>>
+  Description: <textarea name="description" id="description"></textarea>
   Release Date: <input type="text" name="releasedate">
   <input type="hidden" name="albumid" value="<?php echo $_GET['albumid']; ?>">
   <input type="submit">
--- php/addrelease.php
+++ php/addrelease.php
@@ -5,7 +5,7 @@ $albumid = $_GET['albumid'];
 $description = $_GET['description'];
 $releasedate = $_GET['releasedate'];
 
-$sql="INSERT INTO releases (album_id, description, year) VALUES ($albumid, '$description', $releasedate)";
+$sql="INSERT INTO releases (album_id, description, year) VALUES ($albumid, '$description', '$releasedate')";
 
 if (!mysqli_query($con,$sql)) {
   die('Error: ' . mysqli_error($con));
